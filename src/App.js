2567201cdb9ec1b24d9e0a57b6af59937e5f2067
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2022-10-06 22:12:34 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Table from 'react-bootstrap/Table';
 
 function App() {
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    const loadData = async () => {
+      const res = await axios.post('http://demo2211087.mockable.io/mock')
+      if (res.status === 200) {
+        setData(res?.data)
+      }
+    }
+    loadData();
+  }, [])
+  // console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Notary App Assessment</h1>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data?.companies?.map((item, i) => <tr key={i}>
+              <td>{i + 1}</td>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.status}</td>
+            </tr>)
+          }
+        </tbody>
+      </Table>
+    </main>
   );
 }
 
